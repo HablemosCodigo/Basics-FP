9ee8ad402a44dc3c3af36fc2f65691ddcb8aa868
Gabriela Reyes <Gabriela Reyes>
2016-09-12 22:48:27 -0500

    Strings, Variables y formatos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //1.Definir una variable llamada primerNombre que guarde tu nombre.
+
+        String primerNombre = "Michelle";
+
     }
 }
